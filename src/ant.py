19f10a686738994fe66ad1faea00f401ae94b4d4
Sam Hipwell <samhipwell@gmail.com>
2013-11-06 15:21:38 +0000

    Removal of ant and terrain files and places them in main.py as it works better.

--- src/ant.py
+++
@@ -1,6 +0,0 @@
-'''
-Idea Here is to put all the ant code in here
-'''
-class ant():
-    def __init__(self):
-        pass
--- src/main.py
+++ src/main.py
@@ -4,40 +4,63 @@ This will be the main program where the game will run from.
 #lets see now
 import sys
 import pygame
-from terrain import colony
-from ant import ant
+
+
 
 SIZE = (400,400)
 colour = pygame.color.THECOLORS
 
-def game():
-    '''
-    Main game setup
-    '''
-    
-    
-    pygame.init()
-    screen = pygame.display.set_mode((SIZE), 0,32)
-    screen.fill(colour["darkgreen"])
-    clock = pygame.time.Clock()
-    pygame.display.flip()
-    
-    '''
-    Main loop
-    '''
-    while True:
-        # limits the fps
-        time_passed = clock.tick(50)
-        
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                exit_game()
+class ant(object):
+    pass
 
+class colony(object):
+    
+    def __init__(self,pos,size,owner=0):
+        self.pos, self.size = pos, size
+        self.owner = owner
+        self.show(self.owner)
+        
+    def show(self,owner):
+        if self.owner == 0:
+            pygame.draw.circle(screen, colour["white"], self.pos,self.size)
+        if self.owner == 1:
+            pygame.draw.circle(screen, colour["red"], self.pos,self.size)
+        if self.owner == 2:
+            pygame.draw.circle(screen, colour["blue"], self.pos,self.size)
+        if self.owner == 3:
+            pygame.draw.circle(screen, colour["yellow"], self.pos,self.size)   
+            
+    def who_owns(self):
+        return self.owner
+    
+    def where(self):
+        return self.pos
 
 def exit_game():
-    sys.exit()
+    sys.exit()   
+    
+pygame.init()
+screen = pygame.display.set_mode(SIZE)
+screen.fill(colour["darkgreen"])
+clock = pygame.time.Clock()
+col = colony((100,300),10)
+pygame.display.flip()
 
-if __name__ =="__main__":
-    game()
+
+'''
+Main loop
+'''
+while True:
+    # limits the fps
+    time_passed = clock.tick(50)
+    
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            exit_game()
+
+
+
+
+
     
     
