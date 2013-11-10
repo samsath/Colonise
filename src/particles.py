a73bb48cbfc4d1a394b3c062455f5b20d4bea867
Sam Hipwell <samhipwell@gmail.com>
2013-11-10 13:12:04 +0000

    start adding update so that it can be used to move the ant particle

--- src/particles.py
+++ src/particles.py
@@ -1,18 +1,36 @@
 import pygame
 import time
+import math
 
-SIZE=(400,400)
+SIZE=(600,600)
 
 class ant(pygame.sprite.Sprite):
-    def __init__(self,pos,vel):
+    orbit = 10
+    def __init__(self,pos,vel,owner):
+        '''
+        The __init__ will have the position (x,y), velocity (x,y) and the owner int(1-3)
+        '''
         self.pos = pos
         self.vel = vel
+        self.dest = [0,0]
+        self.owner = owner
         self.show(pygame.color.THECOLORS["white"])
         
     def show(self,c):
         pygame.draw.rect(window, c, self.pos,2)
         
-
+    def setdest(self,loc):
+        # this creates the destination for the ant to go to
+        #use math.hypot(x,y) this will get the distance between origin and dest
+        self.dest = loc
+        
+    def update(self):
+        # this works out the distance from the dest then if closer will orbit else move towards
+        if math.hypot((self.pos[0]-self.des[0]),(self.pos[1]-self.des[1])) > orbit:
+            #orbit commnads
+        else:
+            #here to move the ant towards the destination
+            
 
 pygame.init()
 window = pygame.display.set_mode(SIZE)
