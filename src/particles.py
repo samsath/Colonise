4c9f609345ec50894ee8738af92f49d2de48cddd
Phasmicor <phasmicor@gmail.com>
2013-11-15 17:04:06 +0000

    ant.png implementation

--- src/particles.py
+++ src/particles.py
@@ -12,20 +12,21 @@ SIZE=(600,600)
 
 
 class ant(pygame.sprite.Sprite):
-    orbit = 30
-    def __init__(self,pos,vel,owner):
+    orbit = 0
+    def __init__(self,pos,vel,owner,picture):
         '''
         The __init__ will have the position (x,y), velocity (x,y) and the owner int(1-3)
         '''
         self.pos = pos
         self.vel = vel
+        self.image = picture
         self.dest = [0,0]
         self.owner = owner
         #self.show(pygame.color.THECOLORS["white"])
-        self.show( pygame.color.THECOLORS["white"])
+        self.show(self.image)
         
     def show(self,c):
-        pygame.draw.circle(window,c, self.pos,2)
+        window.blit(c,self.pos)
         
     
     def ang(self,pos1,pos2):
@@ -57,10 +58,10 @@ class ant(pygame.sprite.Sprite):
     def update(self):
         # this works out the distance from the dest then if closer will orbit else move towards
         if hypot((self.pos[0]-self.dest[0]),(self.pos[1]-self.dest[1])) > ant.orbit:
-            self.vel = self.ang2(self.dest,self.pos)
-            #posangle = self.ang(self.dest,self.pos)
-            '''
-            old stuff
+           # self.vel = self.ang2(self.dest,self.pos)
+            posangle = self.ang(self.dest,self.pos)
+            
+            #old stuff
             if posangle > 0 and posangle < 90:
                 self.vel = [-1,1]
             elif posangle > 90 and posangle < 180:
@@ -77,32 +78,37 @@ class ant(pygame.sprite.Sprite):
                 self.vel = [0,-1]
             elif posangle == 270:
                 self.vel = [-1,0] 
-            '''
+            
         else:
             # this will be when the particle is orbiting
             # not sure what to do here but owell
-            pass
+			self.vel=[0,0]
         
-        self.show(pygame.color.THECOLORS["black"])
+       # self.show(pygame.color.THECOLORS["black"])
         newpos = [int(self.pos[0]+self.vel[0]),int(self.pos[1]+self.vel[1])]
         self.pos = newpos
-        self.show(pygame.color.THECOLORS["white"])
+      #  self.show(pygame.color.THECOLORS["white"])
             
 
 pygame.init()
 window = pygame.display.set_mode(SIZE)
 window.fill(pygame.color.THECOLORS["black"])
-
-ants = ant((5,5),(0,0),1)
+bg = pygame.image.load('grass.jpg').convert()
+insect = pygame.image.load('ant.png').convert_alpha()
+ants = ant((5,5),(0,0),1,insect)
 pygame.display.flip()
 
 
 
 while True:
     #pygame.time.wait(25) # this adds a 50ms delay to everything
+    #window.blit(bg,(0,0))
     ants.update()
     pygame.display.update()
     event = pygame.event.poll()
+
+    	
+	
     
     
     if event.type == pygame.QUIT:
