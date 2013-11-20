6707a9c4d2a7deb2f912705459db492819ff218c
Sam Hipwell <samhipwell@gmail.com>
2013-11-20 10:13:16 +0000

    edited but not worked, making it all sprity

--- src/col2.py
+++ src/col2.py
@@ -41,15 +41,68 @@ class colony(Sprite):
     
     def show(self):
         colimage = pygame.image.load(colony.baseimage[self.owner]).convert()
+        colSel = pygame.surface(colony.SIZE,pygame.SRCALPHA)
+        colSel.circle = pygame.draw.circle(self.screen,colour["2"],self.pos,colony.SIZE[0])
         if self.state == colony.SELLECT:
-            colSel = pygame.surface(colony.SIZE,pygame.SRCALPHA)
-            colSel = pygame.draw.circle()
             colSel.set_alpha(128)
             
+        elif self.state == colony.UNACTIVE: #maybe else instead
+            colSel.set_alpha(0)
+        self.screen.blit(colimage,self.pos)
+        self.screen.blit(colSel,self.pos)  
+        self.healthdis()
             
-    def update(self,time_passed):
+            
+    def healthcheck(self):
+        if self.health >= 50:
+            return colour["h1"]
+        else
+            return colour["h2"]
+        
+    def healthdis(self):
+        #aim is to display the health bar
+        ocnum = len(self.inhab) # this is the number of inhabitabts
+        healthbg = pygame.surface((colony.SIZE-4)*2, 10),pygame.SRCALPHA)
+        healthbg.fill(colour["ht"])
+        healthbar = pygame.surface(((((colony.SIZE*2)/100)*self.health), 9),pygame.SRCALPHA)
+        healthbar.fill(self.healthcheck())
+        font = pygame.font.Font(None,30)
+        text_suf = font.render(str(ocnum), 1, colour["ht"])
+        tex_pos = (self.pos[0]-colony.SIZE/4,self.pos[1]-colony.SIZE)
+        
+        #writes to screen
+        self.screen.blit(text_suf,tex_pos)
+        self.screen.blit(healthbg,(self.pos[0]-colony.SIZE,self.pos[1]+5))
+        self.screen.blit(healthbar,(self.pos[0]-colony.SIZE,self.pos[1]+5))
+        
+        
+    def update(self ,time_passed):
         if self.state == colony.SELLECT:
             colsurf = pygame.image.load(self.base_image).convert()
             colsurf.set_alpha(50)
             self.screen.blit(colsurf,(self.pos))
             
+            
+    def antenter(self,ant)
+        if self.owner == ant.owner:
+            
+        if len(self.inhab) > 0:
+            del self.inhab[0]
+        else:
+            self.health -= value
+        
+        self.healthdis()
+        if self.health == 0:
+            self.owner = 0
+            self.health = 100
+            
+    def inhabit(self,ant):
+        if self.owner == ant.owner:
+            if self.health < 100:
+                self.health += 1
+                ant.die() # hopefully kill the ant
+            else:
+                self.inhab.append(ant) # hopefully add the ant to the list
+        else:
+            if self.health == 0:
+                self.owner = ant.owner
