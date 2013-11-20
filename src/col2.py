b96c6e0770b588abb60f3ed6a7f6dbf129f0a329
Sam Hipwell <samhipwell@gmail.com>
2013-11-20 15:10:58 +0000

    working on the images and colony look

--- src/col2.py
+++ src/col2.py
@@ -1,7 +1,6 @@
 from __future__ import division
 import os, sys
-from random import randint
-from math import sin, cos, atan2, degrees, pi, hypot
+from random import randrange
 
 import pygame
 from pygame import Rect
@@ -20,17 +19,20 @@ colour = {"0":pygame.color.THECOLORS["white"],
           "h2":pygame.color.THECOLORS["red"]
           } # change this bit to images maybe
 
-class colony(pygame.sprite.Sprite):
-    baseimage = {"0":"colony.png",
-                 "1":"colony1.png",
-                 "2":"colony2.png"}
-    SIZE=(40,40)
+class colony(Sprite):
+    
+    (SELLECT, UNACTIVE) = range(2)
+    
+    baseimage = {"0":"Colony.png",
+                 "1":"Colony1.png",
+                 "2":"Colony2.png"}
+    SIZE=(20,20)
     
     def __init__(self, screen, pos, owner):
-        pygame.sprite.Sprite.__init__(self)
+        Sprite.__init__(self)
         self.screen = screen
         self.pos = pos
-        self.owner = owner
+        self.owner = str(owner)
         self.health = 100 # max 100
         self.inhab = []
         self.state = colony.SELLECT
@@ -47,35 +49,39 @@ class colony(pygame.sprite.Sprite):
             return colour["h2"]
       
     def __str__(self):
-        # returns the information about the col to the computer
+        # returns the information aboself.pos[0]-colony.SIZE[0]/4,ut the col to the computer
         return str(self.owner) + "," + str(self.health) + "," + str(len(self.inhab))  
     
     def show(self):
-        colimage = pygame.image.load(colony.baseimage[self.owner]).convert()
-        colSel = pygame.surface(colony.SIZE,pygame.SRCALPHA)
-        colSel.circle = pygame.draw.circle(self.screen,colour["2"],self.pos,colony.SIZE[0])
+        colimage = pygame.image.load(colony.baseimage[self.owner]).convert_alpha()
+        colimage.get_rect(center=(self.pos))
+        colSel = pygame.Surface(colony.SIZE,pygame.SRCALPHA)
+        pygame.draw.ellipse(colSel,colour[self.owner],[self.pos,colony.SIZE])
+        #colSel.circle = pygame.draw.circle(self.screen,colour["2"],self.pos,colony.SIZE[0])
         if self.state == colony.SELLECT:
             colSel.set_alpha(128)
             
         elif self.state == colony.UNACTIVE: #maybe else instead
             colSel.set_alpha(0)
+        #self.screen.blit(colimage,(self.pos[0]/2,self.pos[1]/2))
         self.screen.blit(colimage,self.pos)
         self.screen.blit(colSel,self.pos)  
         
         #aim is to display the health bar
         ocnum = len(self.inhab) # this is the number of inhabitabts
-        healthbg = pygame.surface(((colony.SIZE-4)*2, 10),pygame.SRCALPHA)
+        
+        healthbg = pygame.Surface((((colony.SIZE[0]-4)*2), 10),pygame.SRCALPHA)
         healthbg.fill(colour["ht"])
-        healthbar = pygame.surface(((((colony.SIZE*2)/100)*self.health), 9),pygame.SRCALPHA)
+        healthbar = pygame.Surface(((((colony.SIZE[0]*2)/100)*self.health), 9),pygame.SRCALPHA)
         healthbar.fill(self.healthcheck())
         font = pygame.font.Font(None,30)
         text_suf = font.render(str(ocnum), 1, colour["ht"])
-        tex_pos = (self.pos[0]-colony.SIZE/4,self.pos[1]-colony.SIZE)
+        tex_pos = (self.pos[0]-colony.SIZE[0]/4,self.pos[1]-colony.SIZE[1])
         
         #writes to screen
         self.screen.blit(text_suf,tex_pos)
-        self.screen.blit(healthbg,(self.pos[0]-colony.SIZE,self.pos[1]+5))
-        self.screen.blit(healthbar,(self.pos[0]-colony.SIZE,self.pos[1]+5))
+        self.screen.blit(healthbg,(self.pos[0]-colony.SIZE[0],self.pos[1]+5))
+        self.screen.blit(healthbar,(self.pos[0]-colony.SIZE[0],self.pos[1]+5))
             
        
             
@@ -118,6 +124,51 @@ class colony(pygame.sprite.Sprite):
             self.des = pos
             
 
+class ant(Sprite):
+    def __init__(self):
+        Sprite.__init__(self)
+        
 
+
+
+pygame.init()
+ant_tick = 0
+col_tick = 0
+
+# list of all the sprite groups
+colony_list = pygame.sprite.Group()
+ant_list = pygame.sprite.Group()
+all_sprite_list = pygame.sprite.Group()
+
+window = pygame.display.set_mode(SIZE)
+
+bg_img = pygame.image.load("grass.jpg").convert()
+
+window.blit(bg_img,(0,0,600,600))
+
+col = colony(window,(randrange(20,SIZE[0]-20),randrange(20, SIZE[1]-20)), 0)
+clock = pygame.time.Clock()
+while True:
+
+    
+    pygame.display.update()
+    event = pygame.event.poll()
+    
+    clock.tick()
+    elapsed = clock.get_time()
+
+    ant_tick += elapsed
+    col_tick += elapsed
+    if ant_tick > 25:
+        ant_tick = 0
+    if col_tick > 1000:
+        col_tick = 0    
+        
+    if event.type == pygame.QUIT:
+        break
+    if event.type == pygame.MOUSEBUTTONDOWN:
+        loc = pygame.mouse.get_pos()
+        col.mouse_click_event(loc)
+pygame.quit()
     
     
