7ccf5435b93ddaa92878c5c68cf6dcc4a2980a8d
Phasmicor <phasmicor@gmail.com>
2013-12-02 00:36:42 +0000

    updated
    redid the levels but some colonies are still close together :/

--- src/Tim's main.py
+++ src/Tim's main.py
@@ -27,7 +27,7 @@ colour = {0:pygame.color.THECOLORS["white"],
 
 levels = {1:"level1.csv",2:"level2.csv",3:"level3.csv",
           4:"level4.csv",5:"level5.csv",6:"level6.csv",
-          7:"level7.csv",8:"level8.csv"} # list of levels as each level will be the a seperate csv file
+          7:"level7.csv",8:"level8.csv",9:"level9.csv"} # list of levels as each level will be the a seperate csv file
 
 baseimage = {0:"Colony.png",
              1:"Colony1.png",
@@ -430,7 +430,7 @@ pygame.init()
 window = pygame.display.set_mode(SIZE)
 pygame.display.set_caption('Colonise','icon.png')
 #insect = pygame.image.load('ant.png').convert_alpha()
-games = game(4)
+games = game(6)
 bg_start = pygame.image.load("openScreen.png")
 window.blit(bg_start,(0,0,600,600))
 pygame.display.flip()
