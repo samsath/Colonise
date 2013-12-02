a48f82b55079855f866794f397d030b1a4c17245
Sam Hipwell <samhipwell@gmail.com>
2013-12-02 23:17:55 +0000

    still playing up with the save to file

--- src/LevelGen2.py
+++ src/LevelGen2.py
@@ -23,17 +23,25 @@ def solve(n):
                     for solution in smaller_solutions
                         if not find(i+1, solution)]
 def run():
-    with open("level" + str(10) + ".csv", "a") as f: #USE CORRECT DIRECTORY
+    with open("level" + str(10) + ".csv", "wb") as f: #USE CORRECT DIRECTORY
         writer = csv.writer(f)
         personal = {1:"att",2:"def"}
         count = 0
+        tobe = []
         for answer in solve(SIZE):
             answers.append(answer)
+        
         choose = choice(answers)
         for i in choose:
             i[0] *= 50
             i[1] *= 50
             string = str(randint(0,3)) + "," + str(i[0]) + "," + str(i[1]) + "," + str(personal[randint(1,2)]) + "," + str(count)
+            tobe.append(string)
             count += 1
-            writer.writerows(string)
         count = 0
+        for i in range(randint(0,12)):
+            row = choice(tobe)
+            print row
+            writer.writerows(row)
+        
+run()
