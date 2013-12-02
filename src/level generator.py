87daaffab8ae2e5729a922deae2b986ebce3a0ea
Phasmicor <phasmicor@gmail.com>
2013-12-02 00:37:03 +0000

    thought i fixed but it isnt - something wrong with for loop

--- src/level generator.py
+++ src/level generator.py
@@ -2,12 +2,12 @@ from random import randint
 from math import hypot
 import csv
 
-number = 8
+number = 9
 bases = 20
 count = 100
 p = 1
-e1 = 5
-e2 = 5
+e1 = 8
+e2 = 8
 arr = [[0,0,0,0,0]]
 nom = bases-p-e1-e2
 i,c,z,v,b = 0,0,0,0,0
@@ -15,7 +15,7 @@ i,c,z,v,b = 0,0,0,0,0
 while i < bases:
     x = randint (50,550)
     y = randint (50,550)
-    for j in arr:
+    for j in range(len(arr)):
         if hypot((x-arr[j][1]),(y-arr[j][2])) < 100: 
             continue
     if z < nom:
